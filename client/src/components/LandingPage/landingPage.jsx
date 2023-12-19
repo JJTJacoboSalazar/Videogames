fabb21afdeb14d9de4367c62e55237a0559a0463
Jacobo <tadeo.jacobo6@gmail.com>
2023-12-18 23:19:29 -0400

    Landing styles

--- client/src/components/LandingPage/landingPage.jsx
+++ client/src/components/LandingPage/landingPage.jsx
@@ -1,13 +1,15 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+
+import style from "./LandingPage.module.css";
 
 const LandingPage = () => {
-  return (
-    <div>
-        <Link role="button" to='/home'>
-        <span>Start</span>
-        </Link>
-    </div>
-  )
+    return (
+        <div className={style.container}>
+            <Link className={style.btn_landing} role="button" to="/home">
+                <span>Start</span>
+            </Link>
+        </div>
+    );
 }
 
-export default LandingPage
+export default LandingPage;
